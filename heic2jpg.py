a67800fac124be0fb47e6620500097619491fd7c
Dong <0x317374@gmail.com>
2021-09-11 05:04:46 +0700

    chore: convert heic to jpg

--- heic2jpg.py
+++ heic2jpg.py
@@ -7,16 +7,16 @@ import os
 def processor_image(filename: str):
     output: str = filename + ".jpg"
 
-    with Image(filename=heic_files[0]) as image:
+    with Image(filename=filename) as image:
         image.format = "jpg"
 
-        image.resize(int(image.width / 2), int(image.height / 2))
+        image.resize(int(image.width / 1.5), int(image.height / 1.5))
         image.save(filename=output)
 
 
 if __name__ == "__main__":
     # Download and unzip: https://drive.google.com/drive/folders/18-FsuqHkyE38udx5gIMTb5hEZhcxVyc4?fbclid=IwAR1TIegRQ1lke7NiR_K9N-TRWCD5C_fImxW-71_EFrw6tjJbcQMWpeTJoLw
-    images_path: str = "C:\\Users\\dccxx\\Downloads\\21-20210910T111338Z-001\\SAO KÊ TỪ THIỆN MIỀN TRUNG 15-10-20 - 06-09-21"
+    images_path: str = "C:\\Users\\dccxx\\Downloads\\21-20210910T111338Z-001"
     heic_files: List[str] = readdir(root_dir=images_path, pattern="**/*.HEIC")
 
     for i in range(0, len(heic_files)):
